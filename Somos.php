e720677db56522380e0a5e096edac28576aec226
Christopher Miranda <chrisMac@ChrisMac.local>
2020-05-30 12:42:24 -0500

    Empezando

---
+++ Somos.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+    <html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title>H&B</title>
+        <link rel="stylesheet" type="text/css" href="code.css">
+    </head>
+    <body>
+    <div class="cabecera" >   
+        <table align="center"> 
+            <tr>
+            <td><a class="botonlogo" href="Proyecto.php" target="_self">H&B </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td> 
+            <td><a class="botoncuenta" href="Proyecto.php" target="_self">Mi Cuenta▾</a></td>
+            </tr>
+        </table>
+    </div><!-- fincabecera-->
+
+    <div class="menu" >   <!-- incio menu-->
+        <table align="center"> 
+        <tr>
+        <td><a class="botonmenu" href="Peluches.php" target="_self">Peluches▾ </a>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td><a class="botonmenu" href="Regalos.php" target="_self">Regalos▾ </a>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td><a class="botonmenu" href="Flores.php" target="_self">Flores▾ </a>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td><a class="botonmenu" href="Golosinas.php" target="_self">Golosinas▾ </a>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        </tr>
+        </table>
+    </div><!-- fin menu-->
+        
+    <div class="letrero">  <!-- incio anuncio--> 
+        <h1>¿Quiénes Somos?</h1>
+    </div><!-- fin anuncio-->
+
+    <div class="mveh">  <!-- incio contenido-->   
+        <table  align="center" CELLSPACING="40" > 
+        <tr>
+            <td>  
+                <h1>Misión</h1><hr>
+                <h2>Brindar los mejores productos y el mejor <br>
+                    servicio en el tema de presentes o detalles <br>
+                    para cualquier ocasión.
+                </h2>
+            </td>
+            <td> 
+                <h1>Visión</h1><hr>
+                <h2>Queremos convertirnos en una empresa en la <br>
+                    que la gente confíe gracias a nuestro trato,<br>
+                    puntualidad, calidad y compromiso</h2>
+            </td><br>
+        </tr>
+        </table><br><br>
+        
+        <table  align="center" CELLSPACING="40" >
+        <tr>
+            <td>
+                <h1>El equipo</h1><hr>
+                <h2>Somos un grupo de personas que trabajan en conjunto para poder brindarte el mejor<br>
+                servicio de entrega de flores, golosinas, peluches y regalos.Esto gracias al conjunto de<br>
+                expertos que tenemos en las diferentes áreas, las cuales van desde jardinería y relleno<br>
+                de peluches, hasta diseño y presentación.</h2><br>
+            </td>
+        </tr>
+        </table>
+        <table  align="center" CELLSPACING="40" >
+        <tr>
+            <td>
+                <h1>Historia</h1><hr>
+                <h2>H&B surgió en el año 2018 con la necesidad de encontrar un buen detalle para para pedirle <br>
+                que fuera mi novia a la que en aquel entonces era mi amiga. Durante la ardua búsqueda del<br>
+                detalle para que la propuesta fuera un éxito, me encontré con varias adversidades; precios <br>
+                demasiado altos, mala calidad de los productos, los envíos tardaban mucho, entre otras <br>
+                más, pero sin duda la adversidad que fue la determinante para crear H&B, fue que para <br>
+                comprar el cartel, los chocolates y las rosas, que fue la idea que más me agrado para la <br>
+                propuesta,  tuve que buscar en sitios diferentes; por lo tanto el costo de envió era mayor, ya <br>
+                que tenía que pagar por separado cada una de las cosas; las cosas no iban a llegar el mismo <br>
+                día, entonces no podía preparar bien el plan de propuesta. Y fue así como decidí comenzar <br>
+                con esta idea. “MH, Director General”.</h2><br>
+
+                <h2>H&B tiene sus oficinas centrales en la CDMX.</h2><br>
+
+                <h2>Actualmente estamos en 20 estados de la República Mexicana y nos distingue nuestra<br>
+                calidad, trato y responsabilidad a la hora de hacer, enviar y entregar nuestros productos.</h2><br><br>
+            </td>
+        </tr>
+        </table>
+    </div>    
+
+    <div class = "casipie"><!-- incio casipie-->
+        <table align="center" CELLSPACING="5" >
+            <tr>
+                <th scope="col"><h1>Desarrolladores</h1></th>
+                <th scope="col"><h1>Contacto</h1></th>
+                <th scope="col"><h1>Conócenos</h1></th>
+                <th scope="col"><h1>Ayúda</h1></th>
+            </tr>
+            <tr>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;HandB@outlook.com&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="botoncontacto" href="Somos.php" target="_self">¿Quiénes Somos? </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;¿Cómo comprar?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            </tr>
+            <tr>
+                <td><a class="botoncontacto" href="https://www.facebook.com/" target="_blank">Abraham García </a></td>
+                <td>A01022398@itesm.mx</td>
+                <td>¿Qué es esto?</td>
+                <td>55-43-92-14-55</td>
+            </tr>
+            <tr>
+                <td><a class="botoncontacto" href="https://www.facebook.com/" target="_blank">Chistopher Miranda </a></td>
+                <td>A01022398@itesm.mx</td>
+                <td>Métodos de Pago</td>
+                <td>Preguntas Frecuentes</td>
+            </tr>
+            <tr>
+                <td><a class="botoncontacto" href="https://www.facebook.com/" target="_blank">Miguel Hernández </a></td>
+                <td>A01022398@itesm.mx</td>
+                <td>Dudas y Sugerencias</td>
+                <td></td>
+            </tr>
+        </table>   
+    </div><!-- fin casipie-->
+
+    <div class="pie"><!-- incio pie-->
+        <p>Politicas de Privacidad | Términos de uso
+        <p>Copyright © 2020 H&B </p>
+    </div><!-- fin pie -->   
+    
+</body> 
+</html> 
