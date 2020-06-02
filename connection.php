0537aa1cd59082365984a17c9f921d938de89772
AbrahamGarcia22 <A01023256@itesm.mx>
2020-06-02 12:11:31 -0500

    prueba

--- connection.php
+++ connection.php
@@ -1,13 +1,14 @@
 <?php
         // Datos para conectar a la base de datos.
-        $nombreServidor = "127.0.0.1:3308";
+        $nombreServidor = "127.0.0.1:3306";
         $nombreUsuario = "adminBD";
-        $passwordBaseDeDatos = "rojo";
-        $nombreBaseDeDatos = "proyecto";
+        $passwordBaseDeDatos = "guardias5522";
+        $nombreBaseDeDatos = "ProyectoFinal4";
         // Crear conexión con la base de datos.
         $conn =new mysqli($nombreServidor, $nombreUsuario, $passwordBaseDeDatos, $nombreBaseDeDatos);
         if(mysqli_connect_errno()){
             echo 'Conexión Fallida : ', mysqli_connect_errno();
             exit();
         }
+
 ?>
