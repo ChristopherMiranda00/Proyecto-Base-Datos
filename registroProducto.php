729fc0e4e2ddd0d0a2f2d2f213b720ac471f5d82
Christopher Miranda <chrisMac@ChrisMac.local>
2020-06-03 01:47:39 -0500

    registro

--- registroProducto.php
+++ registroProducto.php
@@ -5,24 +5,6 @@ session_start();
   }
 require 'connection.php';
 
-  if (!empty($_POST['user']) && !empty($_POST['password'])) {
-      $nombre=$_POST['user'];
-      $password=$_POST['password'];
-      $query=mysqli_query($conn,"SELECT ID_Usuario,Password FROM Usuario where ID_Usuario='".$nombre."' and Password='".$password."' ");
-      $nr =mysqli_num_rows($query);
-      $mensaje='';
-      if($nr==0){
-          $mensaje="Los datos son incorrectos";
-      }
-      else{
-          
-          $_SESSION['username']=$nombre;
-          header("location: ../Proyecto-Base-Datos/Proyecto.php");
-          
-      }
-      
-  }
-
 
 ?>
 <html>
@@ -32,7 +14,7 @@ require 'connection.php';
     <form name="nombre_form" action="Login.php" method="post">
         <div id="Info">
             <label for="uname"><b>Registrar</b></label><br>
-            <a class="btn" href="index.php">Usuario</a><br><br>
+            <a class="btn" href="InsertProducto.php">Usuario</a><br><br>
             <!-- <a href="index.php" type="submit" class="login"  value="Usuario"></a> -->
 
             <label for="psw"><b>Registrar</b></label><br>
