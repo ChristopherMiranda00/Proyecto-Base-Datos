cd5edba973b1f5516d01baa36cce37f8303dc771
miguel7799 <A01022398@itesm.mx>
2020-06-03 06:47:25 -0500

    alineada

--- gracias.php
+++ gracias.php
@@ -22,7 +22,7 @@ require 'connection.php';
 
     <div id="cuerpo">
     <p> 
-<img src="Imagenes/gracias.jpg" align="right">
+<img src="Imagenes/gracias.jpg" aling="center" width="100%">
 </p>
     </div>
     <div class = "casipie"><!-- incio casipie-->
